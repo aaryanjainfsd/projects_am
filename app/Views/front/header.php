4438616ff0f58b241932ff6bf8c0ab38b87643c4
Aaryan Jain <aaryanmanishjain@gmail.com>
2025-03-03 21:54:20 +0530

    photoChanges

--- app/Views/front/header.php
+++ app/Views/front/header.php
@@ -13,12 +13,16 @@
 	<meta name="generator" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home</title>
+
 	<!-- FONTS -->
 	<link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
 	<link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
 	
-	<!-- CSS -->
+	<!-- ICONS  -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+	<!-- CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="<?= ASSETS_CSS ?>slider.css" media="screen">
 	<link rel="stylesheet" href="<?= ASSETS_CSS ?>custom.css" media="screen">
 </head>
