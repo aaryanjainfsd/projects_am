a4846b46499c513c36a073c3e25c07b34b4997b5
Aaryan Jain <aaryanmanishjain@gmail.com>
2025-03-09 19:26:21 +0530

    videoChanges

--- app/Views/front/index.php
+++ app/Views/front/index.php
@@ -137,8 +137,10 @@
 <div class="what_containeredd">
 	<section class="what_we_do">
 		<div class="images_what">
-			<img src="https://plus.unsplash.com/premium_photo-1726783222818-48f7588177d5?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-				class="mainIMg" alt="">
+			<video autoplay muted loop class="mainIMg">
+				<source src="assets/images/10.mp4" type="video/mp4">
+				Your browser does not support the video tag.
+			</video>
 			<div>
 				<img src="assets/images/vector 1.png" class="vectorOne" />
 				<img src="assets/images/vector 2.png" class="vectorTwo" />
